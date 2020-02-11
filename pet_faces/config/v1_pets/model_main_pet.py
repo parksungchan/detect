b7ba9c85b4a119024d94012e59cec9197563ee5a
Paik Jee Hyun <intwis100@naver.com>
2020-02-11 02:02:28 +0000

    models

--- pet_faces/config/v1_pets/model_main_pet.py
+++ pet_faces/config/v1_pets/model_main_pet.py
@@ -3,8 +3,8 @@ import init_value
 
 command = 'python '
 command += init_value.research_dir + '/' + 'object_detection/model_main.py'
-command += ' ' + '--pipeline_config_path=' + init_value.project_dir + '/pet_faces/config/ssd_mobilenet_v1_pets.config'
-command += ' ' + '--model_dir=' + init_value.project_dir + '/pet_faces/output/ckpt'
+command += ' ' + '--pipeline_config_path=' + init_value.project_dir + '/pet_faces/config/v1_pets/ssd_mobilenet_v1_pets.config'
+command += ' ' + '--model_dir=' + init_value.project_dir + '/pet_faces/output/ckpt/v1_pet'
 command += ' ' + '--num_train_steps=50000'
 command += ' ' + '--sample_1_of_n_eval_examples=1'
 command += ' ' + '--alsologtostderr'
