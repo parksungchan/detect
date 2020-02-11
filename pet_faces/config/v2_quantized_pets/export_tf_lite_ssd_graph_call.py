3945a4a509670ecc1d2c78dac8777bfe75c9451e
Paik Jee Hyun <intwis100@naver.com>
2020-02-11 05:00:41 +0000

    models

--- pet_faces/config/v2_quantized_pets/export_tf_lite_ssd_graph_call.py
+++ pet_faces/config/v2_quantized_pets/export_tf_lite_ssd_graph_call.py
@@ -4,7 +4,7 @@ import init_value
 command = 'python '
 command += init_value.research_dir + '/' + 'object_detection/export_tflite_ssd_graph.py'
 command += ' ' + '--pipeline_config_path=' + init_value.project_dir + '/pet_faces/config/v2_quantized_pets/pipeline.config'
-command += ' ' + '--trained_checkpoint_prefix=' + init_value.project_dir + '/pet_faces/output/ckpt/v2_quantized_pet/model.ckpt-145'
+command += ' ' + '--trained_checkpoint_prefix=' + init_value.project_dir + '/pet_faces/output/ckpt/v2_quantized_pet/model.ckpt-1372'
 command += ' ' + '--output_directory=' + init_value.project_dir + '/pet_faces/output/pb/v2_quantized_pet'
 command += ' ' + '--add_postprocessing_op=true'
 subprocess.check_output(command, shell=True)
